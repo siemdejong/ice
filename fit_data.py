bdf7f35c8068f3a290103339276fd34173f67dc7
Siem de Jong <28396796+siemdejong@users.noreply.github.com>
2021-04-28 19:20:46 +0200

    Add number of crystals N

--- fit_data.py
+++ fit_data.py
@@ -36,30 +36,43 @@ def fitting(df):
     df.r3t_err, df.r30_err = r3_err
     print(f"<r>^3 = a*x + b, a={round(r3_opt[0], 2)} +/- {round(r3_err[0], 2)}, b={round(r3_opt[1], 2)} +/- {round(r3_err[1], 2)}.")
 
+    # Number of crystals N.
+    N_opt, N_cov = curve_fit(linear_func, df.times, df.N)
+    N_err = np.sqrt(np.diag(N_cov))
+    df.Nt_opt, df.N0_opt = N_opt
+    df.Nt_err, df.N0_err = N_err
+    print(f"<r>^3 = a*x + b, a={round(N_opt[0], 2)} +/- {round(N_err[0], 2)}, b={round(N_opt[1], 2)} +/- {round(N_err[1], 2)}.")    
+
     return df
 
 def plot(df):
     """Plot the data together with the fit."""
-    fig, axs = plt.subplots(1, 3)
+    fig, axs = plt.subplots(2, 2)
 
     # Ice volume fraction Q.
-    axs[0].set_title("Q")
+    axs[0][0].set_title("Q")
     Q_est = horizontal_func(df.times, df.Q_opt)
-    axs[0].scatter(df.times, df.Q, s=0.8, c='k', label="data")
-    axs[0].plot(df.times, Q_est, 'r', label="fit")
-    axs[0].set_ylim([0, 1])
+    axs[0][0].scatter(df.times, df.Q, s=0.8, c='k', label="data")
+    axs[0][0].plot(df.times, Q_est, 'r', label="fit")
+    axs[0][0].set_ylim([0, 1])
 
     # Mean area A
-    axs[1].set_title("A")
+    axs[0][1].set_title("A")
     A_est = linear_func(df.times, df.At_opt, df.A0_opt)
-    axs[1].scatter(df.times, df.A, s=0.8, c='k', label="data")
-    axs[1].plot(df.times, A_est, 'r', label="fit")
+    axs[0][1].scatter(df.times, df.A, s=0.8, c='k', label="data")
+    axs[0][1].plot(df.times, A_est, 'r', label="fit")
 
     # Mean radius of curvature <r>^3.
-    axs[2].set_title("<r>^3")
+    axs[1][0].set_title("<r>^3")
     r3_est = linear_func(df.times, df.r3t_opt, df.r30_opt)
-    axs[2].scatter(df.times, df.r3, s=0.8, c='k', label="data")
-    axs[2].plot(df.times, r3_est, 'r', label="fit")
+    axs[1][0].scatter(df.times, df.r3, s=0.8, c='k', label="data")
+    axs[1][0].plot(df.times, r3_est, 'r', label="fit")
+
+    # Number of crystals N.
+    axs[1][1].set_title("N")
+    N_est = linear_func(df.times, df.Nt_opt, df.N0_opt)
+    axs[1][1].scatter(df.times, df.N, s=0.8, c='k', label="data")
+    axs[1][1].plot(df.times, N_est, 'r', label="fit")
     # y_err = np.sqrt((xdata * p_err[0])**2 + (p_err[0] * np.std(xdata))**2 + (p_err[1])**2) # Calculate error for linear fits.
 
     # plt.fill_between(xdata, y_est - y_err, y_est + y_err, alpha=0.2)
@@ -71,7 +84,7 @@ def plot(df):
 # root = Tk() # File dialog
 # path = filedialog.askopenfilename(title = "Select data file")
 # root.destroy()
-path = r'E:\Ice\analysis\1uM_T18N_20%_0\1uM_T18N_20%_0.csv'
+path = r'E:\Ice\analysis\0uM_X_10%_0\0uM_X_10%_0.csv'
 
 df = pd.read_csv(path)
 df.times = df.times * 13 / 21.52 # Correct for faster playback speed.
