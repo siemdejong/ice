2cc713880ce0c687638901bbefd8fc27bc151153
Siem de Jong <28396796+siemdejong@users.noreply.github.com>
2021-05-11 15:30:58 +0200

    Update fit_data.py

--- fit_data.py
+++ fit_data.py
@@ -18,36 +18,41 @@ def fitting(df):
     # Ice volume fraction Q.
     Q_opt, Q_cov = curve_fit(horizontal_func, df.times, df.Q)
     Q_err = np.sqrt(np.diag(Q_cov))
-    df.Q_opt = Q_opt[0]
-    df.Q_err = Q_err[0]
+    df['Q_opt'] = Q_opt[0]
+    df['Q_err'] = Q_err[0]
     print(f"Q = {round(Q_opt[0], 3)} +/- {round(Q_err[0], 3)}.")
 
     # Mean area A.
     A_opt, A_cov = curve_fit(linear_func, df.times, df.A)
     A_err = np.sqrt(np.diag(A_cov))
-    df.At_opt, df.A0_opt = A_opt
-    df.At_err, df.A0_err = A_err
+    df['At_opt'], df.A0_opt = A_opt
+    df['At_err'], df.A0_err = A_err
     print(f"Mean area = a*x + b, a={round(A_opt[0], 2)} +/- {round(A_err[0], 2)}, b={round(A_opt[1], 2)} +/- {round(A_err[1], 2)}.")
 
     # Mean radius of curvature <r>^3.
     r3_opt, r3_cov = curve_fit(linear_func, df.times, df.r3)
     r3_err = np.sqrt(np.diag(r3_cov))
-    df.r3t_opt, df.r30_opt = r3_opt
-    df.r3t_err, df.r30_err = r3_err
+    df['r3t_opt'], df.r30_opt = r3_opt
+    df['r3t_err'], df.r30_err = r3_err
     print(f"<r>^3 = a*x + b, a={round(r3_opt[0], 2)} +/- {round(r3_err[0], 2)}, b={round(r3_opt[1], 2)} +/- {round(r3_err[1], 2)}.")
 
     # Number of crystals N.
     N_opt, N_cov = curve_fit(linear_func, df.times, df.N)
     N_err = np.sqrt(np.diag(N_cov))
-    df.Nt_opt, df.N0_opt = N_opt
-    df.Nt_err, df.N0_err = N_err
-    print(f"N = a*x + b, a={round(N_opt[0], 2)} +/- {round(N_err[0], 2)}, b={round(N_opt[1], 2)} +/- {round(N_err[1], 2)}.")    
+    df['Nt_opt'], df.N0_opt = N_opt
+    df['Nt_err'], df.N0_err = N_err
+    print(f"N = a*x + b, a={round(N_opt[0], 2)} +/- {round(N_err[0], 2)}, b={round(N_opt[1], 2)} +/- {round(N_err[1], 2)}.")
+
+    df.drop(['Unnamed: 0'], axis=1)
+    df.to_csv(path)
 
     return df
 
 def plot(df):
     """Plot the data together with the fit."""
     fig, axs = plt.subplots(2, 2)
+
+    fig.suptitle(f'{os.path.splitext(os.path.basename(path))[0]}')
 
     # Ice volume fraction Q.
     axs[0][0].set_title("Q")
@@ -79,14 +84,18 @@ def plot(df):
 
     # plt.yticks(np.linspace(0, 1, 11))
     # plt.legend()
+
+    plt.tight_layout()
+    fig.savefig(os.path.join(os.path.dirname(path), 'fits.png'))
+
     plt.show()
 
-# root = Tk() # File dialog
-# path = filedialog.askopenfilename(title = "Select data file")
-# root.destroy()
-path = r'E:\Ice\analysis\0uM_X_10%_0\0uM_X_10%_0.csv'
+root = Tk() # File dialog
+path = filedialog.askopenfilename(title = "Select data file")
+root.destroy()
+# path = r'E:\Ice\analysis\0uM_X_10%_0\0uM_X_10%_0.csv'
 
-df = pd.read_csv(path)
+df = pd.read_csv(path).dropna() # Drop rows which have at least one NaN.
 df.times = df.times * 13 / 21.52 # Correct for faster playback speed.
 
 df = fitting(df)
