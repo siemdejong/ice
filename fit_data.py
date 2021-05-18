fef9bcd5e693b18ba7816dd5ec8e455c7be074be
Siem de Jong <28396796+siemdejong@users.noreply.github.com>
2021-05-18 17:06:09 +0200

    Change to JMAK equation
    Change to Avrami equation to account for nucleation at the beginning of the measurements. Now almost always uses m=2, but I think this is because of the nature of the data which has many many more data points after transformation.

--- fit_data.py
+++ fit_data.py
@@ -13,34 +13,47 @@ def linear_func(x, a, b):
 def horizontal_func(x, b):
     return 0 * x + b
 
+def jmak_func(t, a, t0, tau, m):
+    return a * (1 - np.exp(-((t - 0) / tau)**m))
+
 def fitting(df, path):
     """Fit through the data."""
     # Ice volume fraction Q.
-    Q_opt, Q_cov = curve_fit(horizontal_func, df.times, df.Q)
+    # Q_opt, Q_cov = curve_fit(horizontal_func, df.times, df.Q)
+    Q_opt, Q_cov = curve_fit(jmak_func, df.times, df.Q, [0.5, 0, 100, 2], bounds=([0, 0, 0, 2], [1, df.times.iat[-1], df.times.iat[-1], 3]), maxfev=1000000)
     Q_err = np.sqrt(np.diag(Q_cov))
     df['Q_opt'] = Q_opt[0]
     df['Q_err'] = Q_err[0]
+    df['t0_opt'] = Q_opt[1]
+    df['t0_err'] = Q_err[1]
+    df['tau_opt'] = Q_opt[2]
+    df['tau_err'] = Q_err[2]
+    df['m_opt'] = Q_opt[2]
+    df['m_err'] = Q_err[2]
     print(f"Q = {round(Q_opt[0], 3)} +/- {round(Q_err[0], 3)}.")
+    print(f"t0 = {round(Q_opt[1], 3)} +/- {round(Q_err[1], 3)}.")
+    print(f"tau = {round(Q_opt[2], 3)} +/- {round(Q_err[2], 3)}.")
+    print(f"m = {round(Q_opt[3], 3)} +/- {round(Q_err[3], 3)}.")
 
     # Mean area A.
     A_opt, A_cov = curve_fit(linear_func, df.times, df.A)
     A_err = np.sqrt(np.diag(A_cov))
-    df['At_opt'], df.A0_opt = A_opt
-    df['At_err'], df.A0_err = A_err
+    df['At_opt'], df['A0_opt'] = A_opt
+    df['At_err'], df['A0_err'] = A_err
     print(f"Mean area = a*x + b, a={round(A_opt[0], 2)} +/- {round(A_err[0], 2)}, b={round(A_opt[1], 2)} +/- {round(A_err[1], 2)}.")
 
     # Mean radius of curvature <r>^3.
     r3_opt, r3_cov = curve_fit(linear_func, df.times, df.r3)
     r3_err = np.sqrt(np.diag(r3_cov))
-    df['r3t_opt'], df.r30_opt = r3_opt
-    df['r3t_err'], df.r30_err = r3_err
+    df['r3t_opt'], df['r30_opt'] = r3_opt
+    df['r3t_err'], df['r30_err'] = r3_err
     print(f"<r>^3 = a*x + b, a={round(r3_opt[0], 2)} +/- {round(r3_err[0], 2)}, b={round(r3_opt[1], 2)} +/- {round(r3_err[1], 2)}.")
 
     # Number of crystals N.
     N_opt, N_cov = curve_fit(linear_func, df.times, df.N)
     N_err = np.sqrt(np.diag(N_cov))
-    df['Nt_opt'], df.N0_opt = N_opt
-    df['Nt_err'], df.N0_err = N_err
+    df['Nt_opt'], df['N0_opt'] = N_opt
+    df['Nt_err'], df['N0_err'] = N_err
     print(f"N = a*x + b, a={round(N_opt[0], 2)} +/- {round(N_err[0], 2)}, b={round(N_opt[1], 2)} +/- {round(N_err[1], 2)}.")
 
     df.to_csv(path, index_label='index')
@@ -55,10 +68,12 @@ def plot(df, path, show=False):
 
     # Ice volume fraction Q.
     axs[0][0].set_title("Q")
-    Q_est = horizontal_func(df.times, df.Q_opt)
-    axs[0][0].scatter(df.times, df.Q, s=0.8, c='k', label="data")
+    # Q_est = horizontal_func(df.times, df.Q_opt)
+    Q_est = jmak_func(df.times, df.Q_opt, df.t0_opt, df.tau_opt, df.m_opt)
+    axs[0][0].scatter(df.times, df.Q, s=0.8, c='k', label="data")    
     axs[0][0].plot(df.times, Q_est, 'r', label="fit")
     axs[0][0].set_ylim([0, 1])
+    # axs[0][0].set_xscale('log')
 
     # Mean area A
     axs[0][1].set_title("A")
@@ -97,7 +112,12 @@ if __name__ == '__main__':
     # path = r'E:\Ice\analysis\0uM_X_10%_0\0uM_X_10%_0.csv'
 
     df = pd.read_csv(path, index_col='index').dropna() # Drop rows which have at least one NaN.
-    df.times = df.times * 13 / 21.52 # Correct for faster playback speed.
+    try: # We only have to correct for the FPS difference once. If df.time_corrected is accessible, it is corrected.
+        df.time_corrected
+    except:
+        df.times = df.times * 13 / 21.52 # Correct for faster playback speed.
+        df['time_corrected'] = True
 
-    df = fitting(df)
-    plot(df)
+    df = fitting(df, path)
+    # print(df.Q_opt.sample(), df.tau_opt.sample(), df.m_opt.sample())
+    plot(df, path, True)
